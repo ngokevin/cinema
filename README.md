1db7fa281e8d5ea12d5f9ee878e6bf01d87c7b89
Kevin Ngo <me@ngokevin.com>
2016-05-11 16:15:42 -0700

    lights, camera, action

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# cinema
+# webvr-cinema
+
 WebVR video player. Inspired by GearVR Netflix and Oculus Video.
---
+++ components/file-input.js
@@ -0,0 +1,10 @@
+AFRAME.registerComponent('file-input', {
+  schema: {
+    event: {default: 'cursor-click'},
+    target: {type: 'selector'}
+  },
+
+  init: function () {
+    var data = this.data;
+  }
+});
